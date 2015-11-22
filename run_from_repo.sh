dce9d2ec40d9d0f2bccdb00dae71aa159ce009e8
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2015-11-22 23:25:54 +0100

    restart required services

--- run_from_repo.sh
+++ run_from_repo.sh
@@ -2,6 +2,7 @@
 set -e
 
 sudo apt-get install -y git python-virtualenv
+cd /opt/
 sudo git clone https://github.com/ahussein/moboware_api.git -b development
 sudo chown -R mobo:mobo moboware_api
 cd moboware_api
@@ -9,8 +10,11 @@ cd moboware_api
 sudo cp config/webservice_uwsgi_upstart.config /etc/init/moboware_api.conf
 sudo cp config/webservice_nginx /etc/nginx/sites-available/moboware_api
 sudo ln -s /etc/nginx/sites-available/moboware_api /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
 
 mkdir -p logs/uwsgi
 
 echo "Starting server"
-sudo start moboware_api
+sudo restart moboware_api
+
+sudo restart nginx
