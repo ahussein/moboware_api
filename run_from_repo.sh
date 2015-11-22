b3f7fe459c52bf526cefd5c23c4436a0fbfd7e32
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2015-11-22 14:21:17 +0100

    update deployments scripts

--- run_from_repo.sh
+++ run_from_repo.sh
@@ -2,8 +2,13 @@
 set -e
 
 sudo apt-get install -y git python-virtualenv
-git clone https://github.com/ahussein/moboware_api.git -b development
+sudo git clone https://github.com/ahussein/moboware_api.git -b development
 sudo chown -R mobo:mobo moboware_api
 cd moboware_api
 ./setup_env.sh
-sudo cp config/webservice_uwsgi_upstart.config /etc/init.d/moboware_api.config
+sudo cp config/webservice_uwsgi_upstart.config /etc/init/moboware_api.conf
+sudo cp config/webservice_nginx /etc/nginx/sites-available/moboware_api
+sudo ln -s /etc/nginx/sites-available/moboware_api /etc/nginx/sites-enabled/
+
+echo "Starting server"
+sudo start moboware_api
--- setup_env.sh
+++ setup_env.sh
@@ -10,6 +10,3 @@ pip install -r requirements.txt
 
 echo "Initialize server"
 python manage.py -i
-
-echo "Starting server"
-uwsgi --http 0.0.0.0:8080 --home venv --wsgi-file webservice/__init__.py --callable app --master
